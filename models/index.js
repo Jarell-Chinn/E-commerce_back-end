7d620b800d0d9716b772fff6fa4ffc1917108bf6
TheLastVelo <jarellchinn44@gmail.com>
2023-08-08 09:31:25 -0700

    index model is complete

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag_products',
+  foreignKey: 'product_id'
+});
+
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: Product,
+  as: 'tag_products',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
